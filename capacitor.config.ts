205699f8bc1493da699a6a74963d229cc550a044
Nikhil Reddy Avuthu <anr.2648@gmail.com>
2022-05-09 19:56:47 +0530

    Initialize capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+    appId: "in.anreddy.test",
+    appName: "test",
+    webDir: "out",
+    bundledWebRuntime: false,
+};
+
+export default config;
